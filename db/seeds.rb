3a559c7222825c242dd366be0cdba33246855290
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-08-10 17:41:54 +0200

    message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+
+  Restaurant.create(name: "Amadeus", address: "Rue Americaine, 12" , phone_number: "04998903884", category: "french" )
+  Restaurant.create(name: "Manhattan Burgers" , address: "Rue de Vleurgat, 1", phone_number: "99483793", category: "belgian" )
+  Restaurant.create(name: "Le Fabiola" , address: "Rue Lesbroussart, 34" , phone_number: "9948723394", category: "italian" )
+  Restaurant.create(name: "Le Shangai", address: "Place Cathédrale" , phone_number: "0974673864", category: "japanese" )
+  Restaurant.create(name: "Bouda Gourmand" , address: "Rue de la résistance" , phone_number: "9478972884747", category: "chinese" )
+
+
+
